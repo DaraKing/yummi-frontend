1529af419163f30cbed24e658e046139c06ef781
DaraKing <dario.knezovich@gmail.com>
2020-05-06 22:20:52 +0200

    finished styling for header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import './Header.css';
 import {
     Link
 } from "react-router-dom";
+import pizza from '../../icons/pizza.svg';
+import { ReactComponent as ShoppingCart } from '../../icons/shopping-cart.svg';
 
 const Header = () => {
     return  (
-        <div>
-            <Link to="/pizzas">Pizzas</Link>
+        <div className="header">
+            <img className="pizza-logo" src={pizza} alt="Pizza" />
+            <div className="navigation">
+                <Link to="/pizzas">Pizzas</Link>
+            </div>
+            <div className="cart-area">
+                <div className="cart-count">1</div>
+                <ShoppingCart className="shopping-cart" />
+            </div>
         </div>
     )
 };
