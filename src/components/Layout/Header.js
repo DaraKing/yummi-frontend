d6e3229b080cd8b0b646825703301f515c3c6b51
DaraKing <dario.knezovich@gmail.com>
2020-05-08 00:19:10 +0200

    added toggle for cart content

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.css';
 import {
     Link
@@ -12,17 +12,35 @@ const mapStateToProps = state => {
 }
 
 const HeaderRender = ({cart}) => {
+
+    const [displayCart, setDisplayCart] = useState(false);
+
+    const renderCartContent = () => {
+        if(displayCart) {
+            return <div className="cart-content"></div>
+        }
+        return ""
+    }
+
+    const toggleCartContent = () => {
+        setDisplayCart(!displayCart)
+    }
+
     return  (
-        <div className="header">
-            <img className="pizza-logo" src={pizza} alt="Pizza" />
-            <div className="navigation">
-                <Link to="/pizzas">Pizzas</Link>
+        <div>
+            <div className="header">
+                <img className="pizza-logo" src={pizza} alt="Pizza" />
+                <div className="navigation">
+                    <Link to="/pizzas">Pizzas</Link>
+                </div>
+                <div className="cart-area">
+                    <div className="cart-count">{cart.length}</div>
+                    <ShoppingCart className="shopping-cart" onClick={() => toggleCartContent()} />
+                </div>
             </div>
-            <div className="cart-area">
-                <div className="cart-count">{cart.length}</div>
-                <ShoppingCart className="shopping-cart" />
-            </div>
+            {renderCartContent()}
         </div>
+
     )
 };
 
