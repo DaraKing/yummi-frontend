36ae56c570be4674c8f21a127117a1751df3a783
DaraKing <dario.knezovich@gmail.com>
2020-05-09 10:57:37 +0200

    added price in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from 'react-redux';
 import "./Cart.css"
 import { removeFromCart } from "../../js/actions";
+import { USD_TO_EUR } from "../../constants";
 
 const mapDispatchToProps = {
     removeFromCart
@@ -9,8 +10,20 @@ const mapDispatchToProps = {
 
 const Cart = (props) => {
 
-    function removePizzaFromCart(data) {
+    const removePizzaFromCart = (data) => {
         props.removeFromCart(data);
+    }
+
+    const totalPrice = () => {
+        let dollars = 0,
+            euros;
+
+        props.items.forEach((item) => {
+            dollars += item.price * item.quantity;
+        })
+
+        euros = dollars*USD_TO_EUR
+        return `${dollars} $ / ${euros.toFixed(2)} €`;
     }
 
     return (
@@ -34,6 +47,19 @@ const Cart = (props) => {
                         </div>
                     )
                 }
+                {
+                    props.items.length !== 0 ? (
+                        <div className="cart-footer">
+                            <div className="proceed-button">
+                                Proceed
+                            </div>
+                            <div className="total-price">
+                                Total price: {totalPrice()}
+                            </div>
+                        </div>
+                    ) : ""
+                }
+
             </div>
         </div>
     )
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 export const API_URL = "http://127.0.0.1:8000/api"
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const USD_TO_EUR = 0.91;
+export const DELIVERY_COST = 5;
